03959040fc3eb20f685e8b3384db2ad73ff441f3
adarsh-dotcom <107241319+adarsh-dotcom@users.noreply.github.com>
2022-07-26 21:37:51 +0530

    Add files via upload

---
+++ lab23june2022/InvalidMeasurementsException.java
@@ -0,0 +1,16 @@
+package lab23june2022;
+
+public class InvalidMeasurementsException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public InvalidMeasurementsException() {
+		System.out.println("Invalid Measurement Exception !!! ");
+		
+	}
+
+	
+}
